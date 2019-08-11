b18465f9fed18af461135d6a8d2edb2fcc66b001
ShunOmine <shun12baseball@gmail.com>
2019-08-11 10:01:25 +0900

    first commit

---
+++ VideoSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoSearchApp
+//
+//  Created by 大嶺舜 on 2019/08/11.
+//  Copyright © 2019 大嶺舜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
