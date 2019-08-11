78d53f4a41f24368fa30a0d1435896dfb049bd2d
ShunOmine <shun12baseball@gmail.com>
2019-08-11 11:31:52 +0900

    finish

--- VideoSearchApp/Views/Search/ViewController.swift
+++ VideoSearchApp/Views/Search/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func searchButton(_ sender: Any) {
+        guard let txt = searchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !txt.isEmpty else {
+            print("不正な検索ワードです。")
+            return
+        }
+        
+        print("検索ワード:\(txt)")
+        let nc = VideoListTableViewController.makeInstance(searchWord: txt)
+        self.navigationController?.pushViewController(nc, animated: true)
+
     }
     
 }
--- VideoSearchApp/Views/VideoList/VideoListTableViewCell.swift
+++ VideoSearchApp/Views/VideoList/VideoListTableViewCell.swift
@@ -6,11 +6,17 @@
 //  Copyright © 2019 大嶺舜. All rights reserved.
 //
 import UIKit
-import Foundation
+import SDWebImage
 
 class VideoListTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var videoImage: UIImageView!
     @IBOutlet weak var countLabel: UILabel!
+    
+    func configure(item: VideoDataEntity) {
+        titleLabel.text = item.title
+        videoImage.sd_setImage(with: URL(string: item.imageUrl))
+        countLabel.text = "\(item.count)"
+    }
     
 }
